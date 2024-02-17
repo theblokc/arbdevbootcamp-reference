c7c6509a69b3175c7cae779ee2d545c3fd3282ed
rn-santos227 <rn_santos227@yahoo.com>
2024-02-17 16:45:48 +0800

    fixed staking and withdrawal function

--- 13-erc20-staking/contracts/MyToken.sol
+++ 13-erc20-staking/contracts/MyToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MyToken is ERC20, Ownable {
   mapping(address => uint256) private _stakes;
   mapping(address => uint256) private _lastStakeTimestamp;
-  uint256 private _rewardRate = 1;
+  uint256 private _rewardRate = 100;
 
   constructor(uint256 initialSupply, address initialOwner) ERC20("MyToken", "MTK") Ownable(initialOwner) {
     _mint(initialOwner, initialSupply);
@@ -17,13 +17,13 @@ contract MyToken is ERC20, Ownable {
     _mint(account, amount);
   }
 
-  function stake(uint256 amount) public {
+  function stake(address account, uint256 amount) public {
     require(amount > 0, "Cannot stake 0 tokens");
     require(balanceOf(msg.sender) >= amount, "Insufficient balance");
 
     _stakes[msg.sender] += amount;
     _lastStakeTimestamp[msg.sender] = block.timestamp;
-    _transfer(msg.sender, address(this), amount);
+    _transfer(account, address(this), amount);
   }
 
   function withdraw() public {
@@ -31,10 +31,10 @@ contract MyToken is ERC20, Ownable {
 
     uint256 stakedAmount = _stakes[msg.sender];
     uint256 reward = (block.timestamp - _lastStakeTimestamp[msg.sender]) * _rewardRate;
-    uint256 totalAmount = stakedAmount + reward;
 
     _stakes[msg.sender] = 0;
-    _transfer(address(this), msg.sender, totalAmount);
+    _transfer(address(this), msg.sender, stakedAmount);
+    _mint(msg.sender, reward);
   }
 
   function getStake(address account) public view returns (uint256) {
--- 13-erc20-staking/scripts/withdraw.js
+++ 13-erc20-staking/scripts/withdraw.js
@@ -1,33 +1,31 @@
 const { expect } = require('chai');
 
 async function main() {
-  const [owner, addr1, addr2] = await ethers.getSigners();
-  const initialSupply = ethers.parseEther('1');
+  const [owner] = await ethers.getSigners();
+  const initialSupply = ethers.parseUnits("1000", 18);
   const myTokenContract = await await hre.ethers.deployContract("MyToken", [initialSupply, owner.address]);
   await myTokenContract.waitForDeployment();
 
-  const gweiAmount = ethers.parseUnits("1000", "gwei");
-  await myTokenContract.connect(owner).mint(addr1.address, gweiAmount);
-  await myTokenContract.connect(owner).mint(addr2.address, gweiAmount);
+  const ethAmount = ethers.parseUnits("10", 18);
+  await myTokenContract.connect(owner).mint(owner.address, ethAmount);
 
-  const gweiAmountStake = ethers.parseUnits("100", "gwei");
-  await myTokenContract.connect(addr1).stake(gweiAmountStake);
-  await myTokenContract.connect(addr2).stake(gweiAmountStake);
+  const ethAmountStake = ethers.parseUnits("10", 18);
+  await myTokenContract.stake(owner.address, ethAmountStake);
 
-  const initialBalance = await myTokenContract.balanceOf(addr1.address);
-  console.log("Addr1 initial balance after withdraw:", initialBalance.toString());
+  const initialBalance = await myTokenContract.balanceOf(owner.address);
+  console.log("Address initial balance before withdraw:", initialBalance.toString());
 
-  const stakedBalance = await myTokenContract.getStake(addr1.address);
-  console.log("Staking successful. Staked balance of addr1:", stakedBalance.toString());
+  const stakedBalance = await myTokenContract.getStake(owner.address);
+  console.log("Staking successful. Staked balance of Address:", stakedBalance.toString());
 
   await new Promise(resolve => setTimeout(resolve, 10000));
-  await myTokenContract.connect(addr1).withdraw();
+  await myTokenContract.connect(owner).withdraw();
 
-  const stakedBalanceAfterWithdrawal = await myTokenContract.getStake(addr1.address);
-  console.log("Withdrawal successful. Staked balance of addr1 after withdrawal:", stakedBalanceAfterWithdrawal.toString())
+  const stakedBalanceAfterWithdrawal = await myTokenContract.getStake(owner.address);
+  console.log("Withdrawal successful. Staked balance of Address after withdrawal:", stakedBalanceAfterWithdrawal.toString())
 
-  const finalBalance = await myTokenContract.balanceOf(addr1.address);
-  console.log("Addr1 final balance after withdraw:", finalBalance.toString());
+  const finalBalance = await myTokenContract.balanceOf(owner.address);
+  console.log("Address final balance after withdraw:", finalBalance.toString());
 }
 
 main().then(() => process.exit(0)).catch(error => {
